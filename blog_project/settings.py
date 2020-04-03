d9c91ea069b709e18e4032cb7d86b3659525adb6
Adan Gutierrez <hevyw8@gmail.com>
2020-04-03 17:28:23 -0500

    added heroku files

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "e#u-ve97tjco$lh))0ke&e@#w-316a2)k!df*cf^t1mg-6&c@p"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
